cdb683bbf9116728f9292f4774a6d6d932498e93
Rachith Anasuri <43659174+Wiryblue@users.noreply.github.com>
2024-10-17 09:32:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,8 @@
-from enum import member
-
 import discord
 from discord import app_commands
 from discord.ext import commands
 import json
 import re
-
-from matplotlib.pyplot import title
 import database as db
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
